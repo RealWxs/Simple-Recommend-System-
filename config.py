7ab49916591dae865f7a7ff28e57c8913e4ebef6
RealWxs <56788520+RealWxs@users.noreply.github.com>
2022-06-03 19:11:58 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {"bandit_strategy": "Thompson",
+          'expire_time': 2,
+          'mysql_port': 3306,
+          'redis_port': 6379,
+          'mysql_user': 'root',
+          'mysql_password': '010808',
+          'mysql_database': 'movielens',
+          'pop_expire_time':1800
+          }
